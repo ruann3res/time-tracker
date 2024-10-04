1ed4a3d71e3cb7aebdceccdf503f6f9814fabb27
ruan.neres <ruan2707neres@gmail.com>
2024-10-04 20:16:14 -0300

    feat: create habit model

---
+++ server/src/model/habit.model.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from 'mongoose';
+
+const HabitSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    completedDates: {
+      type: [Date],
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
+
+export const habitModel = model('Habit', HabitSchema);
